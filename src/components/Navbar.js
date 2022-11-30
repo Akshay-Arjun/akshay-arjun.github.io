686b264bea19d0bfbbc5344aed19735515d1edd5
AKSHAY ARJUN <68991993+PlanetHackers-YT@users.noreply.github.com>
2022-11-30 16:25:04 +0530

    fix navbar not highlighting

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,24 +6,23 @@ import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
 import ContactMailOutlinedIcon from '@mui/icons-material/ContactMailOutlined';
 import LibraryMusicOutlinedIcon from '@mui/icons-material/LibraryMusicOutlined';
 import Paper from '@mui/material/Paper';
+import { Link } from 'react-router-dom';
 export default function Navbar() {
 
-  const [value, setValue] = React.useState(0);
+  const pathname = window.location.pathname; 
+  const [value, setValue] = React.useState(pathname);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
  
   return (
   
       <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-        <BottomNavigation
-          showLabels
-          value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
-        >
-          <BottomNavigationAction label="About" href="/home" icon={<AccountCircleOutlinedIcon />} />
-          <BottomNavigationAction label="Projects" href="/projects" icon={<WorkOutlineIcon />} />
-          <BottomNavigationAction label="Contact" href="/contact" icon={<ContactMailOutlinedIcon />} />
-          <BottomNavigationAction label="Playlist" href="/playlist"icon={<LibraryMusicOutlinedIcon />} />
+        <BottomNavigation value={value} onChange={handleChange} showLabels={true} >
+          <BottomNavigationAction label="About" value="/home" icon={<AccountCircleOutlinedIcon />} component={Link} to='/' />
+          <BottomNavigationAction label="Projects" value="/projects" icon={<WorkOutlineIcon />} component={Link} to='/projects'/>
+          <BottomNavigationAction label="Contact" value="/contact" icon={<ContactMailOutlinedIcon />} component={Link} to='/contact'/>
+          <BottomNavigationAction label="Playlist" value="/playlist"icon={<LibraryMusicOutlinedIcon />} component={Link} to='/playlist'/>
         </BottomNavigation>
 
    
