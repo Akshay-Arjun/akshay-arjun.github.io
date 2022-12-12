cd6b1880b946c469616e8ee7eef98afaeb9143a7
AKSHAY ARJUN <68991993+PlanetHackers-YT@users.noreply.github.com>
2022-12-12 14:56:29 +0530

    Add playlist

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,10 +1,17 @@
+import { Fullscreen } from '@material-ui/icons';
 import React from 'react';
+import Spotify from 'react-spotify-embed';
+
 
 const Playlist = (props) => {
 return (
-    <div>
-      <h2>Playlist</h2>
-    </div>
+  <>
+  <h3> Note : If the selected song doesn't play, click on pause button and then play button.</h3>
+  <h3>Mass Hits :</h3>
+  <Spotify link="https://open.spotify.com/playlist/6CzljxkeQwpRyg08wRGMJb" width={"95%"} frameBorder={10}  />
+  <h3>Love Hits :</h3>
+  <Spotify link="https://open.spotify.com/playlist/3p2tAkOvJqOPYjttRxEQD5" width={"95%"} frameBorder={10}  />
+  </>
   )
 };
 
