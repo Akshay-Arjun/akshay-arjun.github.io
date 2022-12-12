788f4ba984d6a6e1722fa199cb949de3d324a4ae
AKSHAY ARJUN <68991993+PlanetHackers-YT@users.noreply.github.com>
2022-12-12 15:09:03 +0530

    padding + add note about 30 sec

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,4 +1,3 @@
-import { Fullscreen } from '@material-ui/icons';
 import React from 'react';
 import Spotify from 'react-spotify-embed';
 
@@ -6,7 +5,8 @@ import Spotify from 'react-spotify-embed';
 const Playlist = (props) => {
 return (
   <>
-  <h3 style={{paddingRight: '20px'}}> Note : If the selected song doesn't play, click on pause button and then play button.</h3>
+  <h3 style={{alignContent:'space-evenly',paddingRight: '50px'}}> Note : If the selected song doesn't play, click on pause button and then play button.</h3>
+  <h3 style={{paddingRight: '20px'}}> Note : Spotify playlists play only 30 seconds on mobile phone browser.</h3>
   <h3>Mass Hits :</h3>
   <Spotify link="https://open.spotify.com/playlist/6CzljxkeQwpRyg08wRGMJb" width={"95%"} frameBorder={10}  />
   <h3>Love Hits :</h3>
