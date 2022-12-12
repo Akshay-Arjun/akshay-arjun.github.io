2f464101b78b9091948df49cc9e00a96ccd3cb6d
AKSHAY ARJUN <68991993+Akshay-Arjun@users.noreply.github.com>
2022-12-12 15:42:18 +0530

    Update Playlist.js

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -6,10 +6,10 @@ const Playlist = (props) => {
 return (
   <>
   <h3 style={{alignContent:'space-evenly',paddingLeft:"20px",paddingRight: '50px'}}> Note : If the selected song doesn't play, click on pause button and then play button.</h3>
-  <h3 style={{padding: '20px'}}> Note : Spotify playlists play only 30 seconds on mobile phone browser.</h3>
-  <h3 style={{padding: '20px'}}>Mass Hits :</h3>
+  <h3 style={{paddingLeft: '20px',paddingRight: '50px'}}> Note : Spotify playlists play only 30 seconds on mobile phone browser.</h3>
+  <h3 style={{paddingLeft: '20px'}}>Mass Hits :</h3>
   <Spotify link="https://open.spotify.com/playlist/6CzljxkeQwpRyg08wRGMJb" width={"100%"} frameBorder={10}  />
-  <h3 style={{padding: '20px'}}>Love Hits :</h3>
+  <h3 style={{paddingLeft: '20px'}}>Love Hits :</h3>
   <Spotify link="https://open.spotify.com/playlist/3p2tAkOvJqOPYjttRxEQD5" width={"100%"} frameBorder={10}  />
   </>
   )
