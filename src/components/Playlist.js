da596cbb70c75583ac1a2c633654feb1906e7621
AKSHAY ARJUN <68991993+PlanetHackers-YT@users.noreply.github.com>
2022-12-12 15:34:56 +0530

    sdfghj

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -5,8 +5,8 @@ import Spotify from 'react-spotify-embed';
 const Playlist = (props) => {
 return (
   <>
-  <h3 style={{alignContent:'space-evenly',paddingRight: '50px'}}> Note : If the selected song doesn't play, click on pause button and then play button.</h3>
-  <h3 style={{paddingRight: '20px'}}> Note : Spotify playlists play only 30 seconds on mobile phone browser.</h3>
+  <h3 style={{alignContent:'space-evenly',paddingLeft:"20px",paddingRight: '50px'}}> Note : If the selected song doesn't play, click on pause button and then play button.</h3>
+  <h3 style={{padding: '20px'}}> Note : Spotify playlists play only 30 seconds on mobile phone browser.</h3>
   <h3>Mass Hits :</h3>
   <Spotify link="https://open.spotify.com/playlist/6CzljxkeQwpRyg08wRGMJb" width={"95%"} frameBorder={10}  />
   <h3>Love Hits :</h3>
