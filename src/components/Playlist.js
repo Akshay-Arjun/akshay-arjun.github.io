20254f6c867e9cdc174f49d3ebcf228b752ff54a
AKSHAY ARJUN <68991993+PlanetHackers-YT@users.noreply.github.com>
2022-12-12 15:02:11 +0530

    add pad

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -6,7 +6,7 @@ import Spotify from 'react-spotify-embed';
 const Playlist = (props) => {
 return (
   <>
-  <h3> Note : If the selected song doesn't play, click on pause button and then play button.</h3>
+  <h3 style={{paddingRight: '20px'}}> Note : If the selected song doesn't play, click on pause button and then play button.</h3>
   <h3>Mass Hits :</h3>
   <Spotify link="https://open.spotify.com/playlist/6CzljxkeQwpRyg08wRGMJb" width={"95%"} frameBorder={10}  />
   <h3>Love Hits :</h3>
