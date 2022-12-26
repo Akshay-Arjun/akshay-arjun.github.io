a4cbd8183ac4392499560e0d5b8fc988415d6e48
AKSHAY ARJUN <68991993+Akshay-Arjun@users.noreply.github.com>
2022-12-26 12:59:53 +0530

    Update Playlist.js

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -5,7 +5,7 @@ import Spotify from 'react-spotify-embed';
 const Playlist = (props) => {
 return (
   <>
-  <Spotify link="https://open.spotify.com/playlist/0AZGbXgMBJ2fEdufCP1PVv?si=d2iyVdj-RcCv52HqOieI5g" width={"100%"} height={"100vh"} frameBorder={10}  />
+  <Spotify link="https://open.spotify.com/playlist/0AZGbXgMBJ2fEdufCP1PVv?si=d2iyVdj-RcCv52HqOieI5g" width={"100%"} minHeight={"100vh"} frameBorder={10}  />
 
   </>
   )
