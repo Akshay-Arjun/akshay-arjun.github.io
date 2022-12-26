a5bc36c0402dec07949fb422312f90c121e4d882
AKSHAY ARJUN <68991993+Akshay-Arjun@users.noreply.github.com>
2022-12-26 12:58:30 +0530

    Update Playlist.js

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -5,12 +5,8 @@ import Spotify from 'react-spotify-embed';
 const Playlist = (props) => {
 return (
   <>
-  <h3 style={{alignContent:'space-evenly',paddingLeft:"20px",paddingRight: '50px'}}> Note : If the selected song doesn't play, click on pause button and then play button.</h3>
-  <h3 style={{paddingLeft: '20px',paddingRight: '50px'}}> Note : Spotify playlists play only 30 seconds on mobile phone browser.</h3>
-  <h3 style={{paddingLeft: '20px'}}>Mass Hits :</h3>
-  <Spotify link="https://open.spotify.com/playlist/6CzljxkeQwpRyg08wRGMJb" width={"100%"} frameBorder={10}  />
-  <h3 style={{paddingLeft: '20px'}}>Best Melody :</h3>
-  <Spotify link="https://open.spotify.com/playlist/3p2tAkOvJqOPYjttRxEQD5" width={"100%"} frameBorder={10}  />
+  <Spotify link="https://open.spotify.com/playlist/0AZGbXgMBJ2fEdufCP1PVv?si=d2iyVdj-RcCv52HqOieI5g" width={"100%"} height={"100vh"} frameBorder={10}  />
+
   </>
   )
 };
